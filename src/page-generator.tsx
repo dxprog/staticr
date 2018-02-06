1bcacf808fc04c0369e9dcb367441129fa585f9b
Matt Hackmann <matt@dxprog.com>
2018-02-05 19:54:41 -0800

    can now read from multiple sources

--- src/page-generator.tsx
+++ src/page-generator.tsx
@@ -1,28 +1,45 @@
 import * as path from 'path';
 import { renderToStaticMarkup } from 'react-dom/server';
 import * as React from 'react';
-import { ReactNode } from 'react';
+import { ReactNode, ReactElement } from 'react';
+
+import PostsReader from './posts-reader';
 
 import IPost from './interfaces/post';
-import PostsReader from './posts-reader';
 
 import Page from './components/page';
 import Post from './components/post';
 
 export default class PageGenerator {
   private basePath: string;
-  private postsReader: PostsReader;
+  private readers: Array<PostsReader>;
 
   constructor(basePath: string) {
     this.basePath = basePath || process.cwd();
-    this.postsReader = new PostsReader(path.join(this.basePath, 'posts/'));
+    this.readers = [ new PostsReader(path.join(this.basePath, 'posts/')) ];
   }
 
-  build() {
-    return this.postsReader.read().then((posts: Array<IPost>) => {
-      const postsCollection: Array<ReactNode> =
+  public build(): Promise<void> {
+    return this.readPosts().then((posts: Array<IPost>) => {
+      const postsCollection: Array<ReactElement<Post>> =
         posts.map((post: IPost, index: number) => <Post post={post} key={`post-${index}`} />);
       console.log(renderToStaticMarkup(<Page title="Hello, world" bodyNodes={postsCollection} />));
     });
+  }
+
+  /**
+   * Invokes all PostsReaders and returns a flat array of IPost objects
+   */
+  private readPosts(): Promise<Array<IPost>> {
+    const readers = this.readers.map((postReader: PostsReader) => postReader.read());
+    return Promise.all(readers).then((posts: Array<Array<IPost>>) => {
+      return posts.reduce((allPosts: Array<IPost>, readerPosts: Array<IPost>) => {
+        return [ ...allPosts, ...readerPosts ];
+      }, []);
+    });
+  }
+
+  public addReader(reader: PostsReader) {
+    this.readers.push(reader);
   }
 };
