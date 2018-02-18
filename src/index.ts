e261ea01656bac672dfe882283a28fcaafdd7ab6
Matt Hackmann <matt@dxprog.com>
2018-02-17 23:19:50 -0800

    renderers are exported

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,14 @@ export * from './interfaces/post';
 export * from './interfaces/rendered-page';
 export * from './interfaces/renderer';
 export * from './interfaces/content-reader';
+
+import { IPageRenderer } from './interfaces/page-renderer';
+import { IRenderer } from './interfaces/renderer';
+import { PostsRollupRenderer } from './renderers/posts-rollup-renderer';
+import { PostRenderer } from './renderers/post-renderer';
+import { PageRenderer } from './renderers/page-renderer';
+export const Renderers = {
+  PageRenderer,
+  PostRenderer,
+  PostsRollupRenderer,
+};
